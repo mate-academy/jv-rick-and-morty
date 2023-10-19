3a0ab9970603807051fd3eb82d6a57a56753da89
Dmytro Martyshchuk <Di.mart.ap@gmail.com>
2023-10-19 16:54:35 +0300

    Replace HttpClient with FeinClient

--- src/main/java/mate/academy/rickandmorty/config/DataFetchConfig.java
+++ src/main/java/mate/academy/rickandmorty/config/DataFetchConfig.java
@@ -1,19 +1,12 @@
 package mate.academy.rickandmorty.config;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import mate.academy.rickandmorty.client.RickAndMortyClient;
 import mate.academy.rickandmorty.dto.external.CharacterResponseDataDto;
 import mate.academy.rickandmorty.dto.external.ExternalCharacterDto;
 import mate.academy.rickandmorty.service.CharacterService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
@@ -21,41 +14,22 @@ import org.springframework.context.event.EventListener;
 @RequiredArgsConstructor
 @Configuration
 public class DataFetchConfig {
-    private final CharacterService service;
-    private final ObjectMapper objectMapper;
-    @Value("${rickandmorty.api.url}")
-    private String characterUrl;
+    private static final Long firstPage = 1L;
+    private final RickAndMortyClient rickAndMortyClient;
+    private final CharacterService characterService;
 
     @EventListener(ApplicationReadyEvent.class)
     public void fetchData() {
-        List<ExternalCharacterDto> characters = new ArrayList<>();
-        CharacterResponseDataDto response = loadData(characterUrl);
+        List<ExternalCharacterDto> externalCharacterDtos = new ArrayList<>();
+
+        long currentPage = firstPage;
+        CharacterResponseDataDto charactersData;
         do {
-            characters.addAll(response.results());
-            response = loadData(response.info().next());
-        } while (response.info().next() != null);
-        service.saveAll(characters);
-    }
+            charactersData = rickAndMortyClient.getCharactersData(currentPage);
+            externalCharacterDtos.addAll(charactersData.results());
+            currentPage++;
+        } while (currentPage <= charactersData.info().pages());
 
-    private CharacterResponseDataDto loadData(String url) {
-        HttpClient httpClient = HttpClient.newHttpClient();
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(url))
-                .build();
-
-        try {
-            HttpResponse<byte[]> response = httpClient.send(
-                    request,
-                    HttpResponse.BodyHandlers.ofByteArray()
-            );
-            return objectMapper.readValue(
-                    response.body(), new TypeReference<>() {
-                    }
-            );
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        characterService.saveAll(externalCharacterDtos);
     }
 }
