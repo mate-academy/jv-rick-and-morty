182f2e8c1858b0d0da4d8b900c01290a750564aa
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-10-23 16:03:46 +0300

    Separated logic getCharacters into 2 separated methods Renamed CustomThread into GetRequestDtoThread Replaced for cycle by IntStream.range()

--- src/main/java/mate/academy/rickandmorty/service/impl/CharacterClient.java
+++ src/main/java/mate/academy/rickandmorty/service/impl/CharacterClient.java
@@ -1,62 +1,59 @@
 package mate.academy.rickandmorty.service.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
 import mate.academy.rickandmorty.dto.external.RequestDto;
 import mate.academy.rickandmorty.dto.external.RequestResultsDto;
-import mate.academy.rickandmorty.thread.CustomThread;
+import mate.academy.rickandmorty.thread.GetRequestDtoThread;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class CharacterClient {
-    private static final String BASE_URL = "https://rickandmortyapi.com/api/character/?page=";
     private static final int INITIAL_PAGE = 1;
 
     private final ObjectMapper objectMapper;
 
     public List<RequestResultsDto> getCharacters() {
         HttpClient httpClient = HttpClient.newHttpClient();
-        List<RequestResultsDto> characters;
-        int pages;
+        List<RequestResultsDto> characters = new ArrayList<>();
+        int pages = getPagesCountAndFirstPageResults(httpClient, characters);
+        completeCharacterList(httpClient, characters, pages);
+        return characters;
+    }
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(BASE_URL + INITIAL_PAGE))
-                .build();
-        try {
-            HttpResponse<String> response = httpClient.send(
-                    request, HttpResponse.BodyHandlers.ofString());
-            RequestDto dataDto = objectMapper.readValue(
-                    response.body(), RequestDto.class);
-            pages = dataDto.info().pages();
-            characters = new ArrayList<>(dataDto.results());
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    private int getPagesCountAndFirstPageResults(
+            HttpClient httpClient, List<RequestResultsDto> characters) {
+        RequestDto firstPage =
+                new GetRequestDtoThread(httpClient, objectMapper, INITIAL_PAGE).call();
+        characters.addAll(firstPage.results());
+        return firstPage.info().pages();
+    }
+
+    private void completeCharacterList(
+            HttpClient httpClient, List<RequestResultsDto> characters, int pages) {
         List<CompletableFuture<List<RequestResultsDto>>> futureRequests = new ArrayList<>();
-        for (int i = INITIAL_PAGE + 1; i <= pages; i++) {
-            int page = i;
-            CompletableFuture<List<RequestResultsDto>> completableFuture =
-                    CompletableFuture.supplyAsync(() ->
-                            new CustomThread(httpClient, objectMapper, page).call()
-            );
-            futureRequests.add(completableFuture);
-        }
+        futureRequests.add(CompletableFuture.supplyAsync(() ->
+                IntStream.range(INITIAL_PAGE + 1, pages + 1)
+                    .mapToObj(page ->
+                        CompletableFuture.supplyAsync(() ->
+                            new GetRequestDtoThread(httpClient, objectMapper, page).call()))
+                    .map(requestDtoCompletableFuture ->
+                        requestDtoCompletableFuture.join().results())
+                    .flatMap(List::stream)
+                    .collect(Collectors.toList())
+        ));
+
         List<RequestResultsDto> additionalCharacters = futureRequests.stream()
                 .map(CompletableFuture::join)
                 .flatMap(List::stream)
                 .toList();
-
         characters.addAll(additionalCharacters);
-        return characters;
     }
 }
--- src/main/java/mate/academy/rickandmorty/thread/GetRequestDtoThread.java
+++ src/main/java/mate/academy/rickandmorty/thread/GetRequestDtoThread.java
@@ -6,21 +6,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.List;
 import java.util.concurrent.Callable;
 import lombok.RequiredArgsConstructor;
 import mate.academy.rickandmorty.dto.external.RequestDto;
-import mate.academy.rickandmorty.dto.external.RequestResultsDto;
 
 @RequiredArgsConstructor
-public class CustomThread implements Callable<List<RequestResultsDto>> {
+public class GetRequestDtoThread implements Callable<RequestDto> {
     private static final String BASE_URL = "https://rickandmortyapi.com/api/character/?page=";
     private final HttpClient httpClient;
     private final ObjectMapper objectMapper;
     private final int pageNumber;
 
     @Override
-    public List<RequestResultsDto> call() {
+    public RequestDto call() {
         HttpRequest pageRequest = HttpRequest.newBuilder()
                 .GET()
                 .uri(URI.create(BASE_URL + pageNumber))
@@ -28,8 +26,7 @@ public class CustomThread implements Callable<List<RequestResultsDto>> {
         try {
             HttpResponse<String> response = httpClient.send(
                     pageRequest, HttpResponse.BodyHandlers.ofString());
-            RequestDto dataDto = objectMapper.readValue(response.body(), RequestDto.class);
-            return dataDto.results();
+            return objectMapper.readValue(response.body(), RequestDto.class);
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
