ce40fea07c36e62e25583926e9ebf922fa89854a
Ros <traine41@icloud.com>
2023-10-26 19:14:50 +0100

    created Character repository

---
+++ src/main/java/mate/academy/rickandmorty/repository/CharacterRepository.java
@@ -0,0 +1,11 @@
+package mate.academy.rickandmorty.repository;
+
+import java.util.List;
+import mate.academy.rickandmorty.model.CharacterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<CharacterEntity, Long> {
+    List<CharacterEntity> findByNameContaining(String name);
+}
