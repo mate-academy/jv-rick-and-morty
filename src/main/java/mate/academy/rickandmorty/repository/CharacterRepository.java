3f174b4680accdac18498474e652fc7bfcdfa6a9
literandltx <romanskovronn@gmail.com>
2023-11-01 22:01:32 +0200

    add character repo

---
+++ src/main/java/mate/academy/rickandmorty/repository/CharacterRepository.java
@@ -0,0 +1,7 @@
+package mate.academy.rickandmorty.repository;
+
+import mate.academy.rickandmorty.model.Character;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CharacterRepository extends JpaRepository<Character, Long> {
+}
