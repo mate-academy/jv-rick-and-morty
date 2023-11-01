012ac03f517b6a96ffd89ee5425275df3ab3d770
literandltx <romanskovronn@gmail.com>
2023-11-01 23:28:07 +0200

    checkstyle cleanup

--- src/main/java/mate/academy/rickandmorty/mapper/CharacterMapper.java
+++ src/main/java/mate/academy/rickandmorty/mapper/CharacterMapper.java
@@ -4,8 +4,8 @@ import mate.academy.rickandmorty.config.MapperConfig;
 import mate.academy.rickandmorty.dto.external.CharacterDto;
 import mate.academy.rickandmorty.dto.internal.CharacterRespondDto;
 import mate.academy.rickandmorty.model.Character;
-import org.mapstruct.Mapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class)
 public interface CharacterMapper {
