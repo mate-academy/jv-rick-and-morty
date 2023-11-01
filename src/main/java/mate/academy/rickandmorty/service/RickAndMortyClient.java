a525feb44fd3ea79062701c340dc4d71527f473b
literandltx <romanskovronn@gmail.com>
2023-11-01 22:00:22 +0200

    add client

---
+++ src/main/java/mate/academy/rickandmorty/service/RickAndMortyClient.java
@@ -0,0 +1,50 @@
+package mate.academy.rickandmorty.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import mate.academy.rickandmorty.dto.external.CharacterDto;
+import mate.academy.rickandmorty.dto.external.ListOfCharacterResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RickAndMortyClient {
+    private static final String URL = "https://rickandmortyapi.com/api/character";
+    private final ObjectMapper objectMapper;
+
+    public ListOfCharacterResponseDto fetchPage(String url) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(url))
+                .build();
+        try {
+            String body = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+            return objectMapper.readValue(body, ListOfCharacterResponseDto.class);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public List<CharacterDto> fetchAllPages() {
+        String url = URL;
+        List<CharacterDto> accum = new ArrayList<>();
+
+        do {
+            ListOfCharacterResponseDto fetchPage = fetchPage(url);
+
+            url = fetchPage.getInfo().getNext();
+            accum.addAll(fetchPage.getCharacters());
+        } while (url != null);
+
+        return accum;
+    }
+
+}
