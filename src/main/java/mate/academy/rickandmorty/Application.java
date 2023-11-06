e98828f6afa3273d9a529a1539c9155d08c9d582
Oksana Rudenko <0ksaha_rudenko@ukr.net>
2023-11-06 17:30:27 +0200

    rick and morti fixed running

--- src/main/java/mate/academy/rickandmorty/Application.java
+++ src/main/java/mate/academy/rickandmorty/Application.java
@@ -2,8 +2,10 @@ package mate.academy.rickandmorty;
 
 import mate.academy.rickandmorty.service.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -16,6 +18,10 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        characterService.saveAllToDB();
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> characterService.saveAllToDB();
     }
 }
