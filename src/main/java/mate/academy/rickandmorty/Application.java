5ea953a95966a34778dce8b7f5b50f4d9f9171c8
Oksana Rudenko <0ksaha_rudenko@ukr.net>
2023-11-06 21:57:40 +0200

    rick and morti injected bean

--- src/main/java/mate/academy/rickandmorty/Application.java
+++ src/main/java/mate/academy/rickandmorty/Application.java
@@ -1,7 +1,6 @@
 package mate.academy.rickandmorty;
 
 import mate.academy.rickandmorty.service.CharacterService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,19 +8,12 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-    private static CharacterService characterService;
-
-    @Autowired
-    public Application(CharacterService characterService) {
-        Application.characterService = characterService;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner() {
+    public CommandLineRunner commandLineRunner(CharacterService characterService) {
         return args -> characterService.saveAllToDB();
     }
 }
