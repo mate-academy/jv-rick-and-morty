d799aee1765b06c72344efcf2f1f5ff0bf6af687
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-11-07 02:02:02 +0200

    added swagger, fully implemented task

--- src/main/java/mate/academy/rickandmorty/controller/CharacterController.java
+++ src/main/java/mate/academy/rickandmorty/controller/CharacterController.java
@@ -1,5 +1,7 @@
 package mate.academy.rickandmorty.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import mate.academy.rickandmorty.dto.internal.CharacterDto;
@@ -9,22 +11,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Rick and Morty API")
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
 public class CharacterController {
     private CharacterService characterService;
 
+    @Operation(summary = "Get all available characters.")
     @GetMapping
     public List<Character> getCharacters() {
         return characterService.getAll();
     }
 
+    @Operation(summary = "Find characters by specific name.")
     @GetMapping("/search")
     public List<CharacterDto> getCharactersByName(String name) {
         return characterService.getCharactersByName(name);
     }
 
+    @Operation(summary = "Generate and save random existing character.")
     @GetMapping("/random")
     public Character saveRandomCharacter() {
         Character character = characterService.saveRandomCharacter();
