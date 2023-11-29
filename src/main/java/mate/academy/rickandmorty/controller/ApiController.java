9ea5d8e622804437457198fa4d8a0319ecf3e583
khorkovenko <khorkovenko>
2023-11-29 12:16:56 +0200

    added correct service, deleted controller wastefull method

--- src/main/java/mate/academy/rickandmorty/controller/ApiController.java
+++ src/main/java/mate/academy/rickandmorty/controller/ApiController.java
@@ -2,24 +2,17 @@ package mate.academy.rickandmorty.controller;
 
 import lombok.RequiredArgsConstructor;
 import mate.academy.rickandmorty.model.CharacterEntity;
-import mate.academy.rickandmorty.service.CharacterService;
+import mate.academy.rickandmorty.service.RickAndMortyClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ApiController {
-    private final CharacterService characterService;
+    private final RickAndMortyClient rickAndMortyClient;
 
     @GetMapping("/characters")
-    public CharacterEntity getRandomCharacter(@RequestParam Long id) {
-        return characterService.findById(id);
+    public CharacterEntity getRandomCharacter() {
+        return rickAndMortyClient.makeRequestRandomCharacter();
     }
-
-    @GetMapping("/external-id")
-    public CharacterEntity getCharactersByExternalId(@RequestParam String externalId) {
-        return characterService.findByExternalId(externalId);
-    }
-
 }
