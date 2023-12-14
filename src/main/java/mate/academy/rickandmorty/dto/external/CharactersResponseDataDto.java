b2c732a9ae80044bcbdbc415e6ccae38eeed7425
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-12-14 18:36:39 +0200

    Added result list to CharacterResponseDataDto

--- src/main/java/mate/academy/rickandmorty/dto/external/CharactersResponseDataDto.java
+++ src/main/java/mate/academy/rickandmorty/dto/external/CharactersResponseDataDto.java
@@ -1,6 +1,8 @@
 package mate.academy.rickandmorty.dto.external;
 
+import java.util.List;
+
 public class CharactersResponseDataDto {
     private CharactersInfo info;
-
+    private List<CharacterResponseInfoDto> results;
 }
