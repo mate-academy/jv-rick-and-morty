7b01ad20a7fdd2ea7215b244ef21e4ea623dd649
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-12-14 20:43:46 +0200

    Implemented getAllCharacyers method to client

--- src/main/java/mate/academy/rickandmorty/client/RickAndMortyApiClient.java
+++ src/main/java/mate/academy/rickandmorty/client/RickAndMortyApiClient.java
@@ -2,6 +2,7 @@ package mate.academy.rickandmorty.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import mate.academy.rickandmorty.dto.external.CharacterResponseInfoDto;
 import mate.academy.rickandmorty.dto.external.CharactersResponseDataDto;
 import org.springframework.stereotype.Component;
 
@@ -10,19 +11,37 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Component
 public class RickAndMortyApiClient {
-    private static final String BASIC_CHARACTERS_URL = "https://rickandmortyapi.com/api/character";
     private static final String PAGEABLE_CHARACTERS_URL = "https://rickandmortyapi.com/api/character/?page=%s";
     private final ObjectMapper objectMapper;
+    private final HttpClient httpClient = HttpClient.newHttpClient();
 
-    public CharactersResponseDataDto getBasicCharactersData() {
-        HttpClient httpClient = HttpClient.newHttpClient();
+    public List<CharacterResponseInfoDto> getAllCharacters() {
+        int pageCounter = 1;
+        CharactersResponseDataDto firstPageCharacterData = getCharactersDataByPage(pageCounter);
+        List<CharacterResponseInfoDto> characters = new ArrayList<>(firstPageCharacterData.getResults());
+
+        while (pageCounter <= firstPageCharacterData.getInfo().getPages()) {
+            pageCounter++;
+
+            CharactersResponseDataDto charactersDataByPage = getCharactersDataByPage(pageCounter);
+            characters.addAll(charactersDataByPage.getResults());
+        }
+
+        return characters;
+    }
+
+    private CharactersResponseDataDto getCharactersDataByPage(int pageNumber) {
+        String url = PAGEABLE_CHARACTERS_URL.formatted(pageNumber);
+
         HttpRequest getBasicInfoRequest = HttpRequest.newBuilder()
                 .GET()
-                .uri(URI.create(BASIC_CHARACTERS_URL))
+                .uri(URI.create(url))
                 .build();
 
         try {
@@ -36,7 +55,7 @@ public class RickAndMortyApiClient {
                     CharactersResponseDataDto.class
             );
         } catch (IOException | InterruptedException e) {
-            throw new RuntimeException("Can't get basic info", e);
+            throw new RuntimeException("Can't get info about characters on page: " + pageNumber, e);
         }
     }
 }
