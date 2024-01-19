111e92ddbf54ed5126440355c82b2855425d9084
Andrew Hudzenko <s1mpleworkpost@gmail.com>
2024-01-19 20:20:25 +0200

    add openapi

--- src/main/java/mate/academy/rickandmorty/controller/CharacterController.java
+++ src/main/java/mate/academy/rickandmorty/controller/CharacterController.java
@@ -1,5 +1,7 @@
 package mate.academy.rickandmorty.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.rickandmorty.dto.internal.CharacterWikiDto;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Rick and Morty", description = "Endpoints for retrieving some characters")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/rickAndMorty/character")
@@ -17,11 +20,15 @@ public class CharacterController {
     private final CharacterService characterService;
 
     @GetMapping("/random")
+    @Operation(summary = "Get random character",
+            description = "Get random character from internal db")
     public CharacterWikiDto generateRandomCharacter() {
         return characterService.generateRandomCharacter();
     }
 
     @GetMapping("/search")
+    @Operation(summary = "Get a list of characters by argument",
+            description = "Get a list of characters by incoming name argument")
     public List<CharacterWikiDto> searchCharactersByNameArgument(
             @RequestParam String nameArgument
     ) {
