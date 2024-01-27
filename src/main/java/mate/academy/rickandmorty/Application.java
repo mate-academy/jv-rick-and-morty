2f300344dce3fa32026eb321c0c96c25dcc617ca
Artem Peiev <slaybrute@gmail.com>
2024-01-27 05:33:46 +0200

    add possibility to save characters from api to db and work with them

--- src/main/java/mate/academy/rickandmorty/Application.java
+++ src/main/java/mate/academy/rickandmorty/Application.java
@@ -1,12 +1,24 @@
 package mate.academy.rickandmorty;
 
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import mate.academy.rickandmorty.service.server.CharacterServer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class Application {
+    private static CharacterServer characterServer;
 
-    public static void main(String[] args) {
+    @Autowired
+    public void setCharacterServer(CharacterServer characterServer1) {
+        characterServer = characterServer1;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         SpringApplication.run(Application.class, args);
+        characterServer.saveAllFromExternalApi();
     }
 }
