d41aa770c23d60616efd0494ef9de384727e176d
Artem Peiev <slaybrute@gmail.com>
2024-01-27 17:57:44 +0200

    removed controller with external api

--- src/main/java/mate/academy/rickandmorty/dto/external/episode/EpisodeSearchParameters.java
+++
@@ -1,9 +0,0 @@
-package mate.academy.rickandmorty.dto.external.episode;
-
-import lombok.Data;
-
-@Data
-public class EpisodeSearchParameters {
-    private String name;
-    private String episode;
-}
