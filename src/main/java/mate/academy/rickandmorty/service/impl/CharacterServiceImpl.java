5f26c84b1f8bf14a25192cf6279e82470996517f
Danil Ponomarenko <akame.dante13@gmail.com>
2024-02-07 19:08:37 +0200

    Implemented CharacterServiceImpl

---
+++ src/main/java/mate/academy/rickandmorty/service/impl/CharacterServiceImpl.java
@@ -0,0 +1,28 @@
+package mate.academy.rickandmorty.service.impl;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import mate.academy.rickandmorty.dto.internal.CharacterDto;
+import mate.academy.rickandmorty.mapper.CharacterMapper;
+import mate.academy.rickandmorty.repository.CharacterRepository;
+import mate.academy.rickandmorty.service.CharacterService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CharacterServiceImpl implements CharacterService {
+    private final CharacterRepository characterRepository;
+    private final CharacterMapper characterMapper;
+
+    @Override
+    public CharacterDto getRandomCharacter() {
+        return characterMapper.toDto(characterRepository.getRandomCharacter());
+    }
+
+    @Override
+    public List<CharacterDto> getCharacterListByName(String name) {
+        return characterRepository.findAllByName(name).stream()
+                .map(characterMapper::toDto)
+                .toList();
+    }
+}
