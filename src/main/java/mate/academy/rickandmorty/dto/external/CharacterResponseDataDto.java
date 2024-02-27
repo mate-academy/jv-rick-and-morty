187093a8116347c346a9b48a79eff5eb62fb45f5
Solomia Bobyak <mia.bobyak@gmail.com>
2024-02-27 11:13:06 +0200

    fixed CharacterResponseDataDto

--- src/main/java/mate/academy/rickandmorty/dto/external/CharacterResponseDataDto.java
+++ src/main/java/mate/academy/rickandmorty/dto/external/CharacterResponseDataDto.java
@@ -3,11 +3,9 @@ package mate.academy.rickandmorty.dto.external;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
-import mate.academy.rickandmorty.dto.internal.CharacterDto;
 
 @Getter
 @Setter
 public class CharacterResponseDataDto {
-    private Long externalId;
-    private List<CharacterDto> results;
+    private List<CharacterDataDto> results;
 }
