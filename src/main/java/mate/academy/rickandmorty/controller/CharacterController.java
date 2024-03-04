7dfcb6a042e24e36e77306e95a15fd812fc24b32
Yaroslav Boiko <48257427+araslav@users.noreply.github.com>
2024-03-04 16:28:22 +0200

    Add files via upload
    Character class controller

---
+++ src/main/java/mate/academy/rickandmorty/controller/CharacterController.java
@@ -0,0 +1,29 @@
+package mate.academy.rickandmorty.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import mate.academy.rickandmorty.model.Character;
+import mate.academy.rickandmorty.service.CharacterService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Character controller", description = "Endpoints for managing characters")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/characters")
+public class CharacterController {
+    private final CharacterService characterService;
+
+    @Operation(summary = "Find characters by name.",
+            parameters = {@Parameter(required = true,
+                    description = "Name of a character. Allows low case name.")})
+    @GetMapping("/find")
+    public List<Character> findByName(@RequestParam String name) {
+        return characterService.findAllByName(name);
+    }
+}
