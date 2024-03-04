1e23da058dd39c722ca8e5c2aabae1de8340bd3c
Yaroslav Boiko <yaroslav.boiko@ingenios.ro>
2024-03-04 16:13:11 +0200

    Added swagger in pom and annotations

--- src/main/java/mate/academy/rickandmorty/controller/CharacterController.java
+++ src/main/java/mate/academy/rickandmorty/controller/CharacterController.java
@@ -1,6 +1,10 @@
 package mate.academy.rickandmorty.controller;
 
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import mate.academy.rickandmorty.model.Character;
 import mate.academy.rickandmorty.service.CharacterService;
@@ -9,12 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Character controller", description = "Endpoints for managing characters")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/characters")
 public class CharacterController {
     private final CharacterService characterService;
 
+    @Operation(summary = "Find characters by name.",
+            parameters = {@Parameter(required = true, description = "Name of a character. Allows low case name.")})
     @GetMapping("/find")
     public List<Character> findByName(@RequestParam String name) {
         return characterService.findAllByName(name);
--- src/main/java/mate/academy/rickandmorty/repository/CharacterRepository.java
+++ src/main/java/mate/academy/rickandmorty/repository/CharacterRepository.java
@@ -3,9 +3,11 @@ package mate.academy.rickandmorty.repository;
 import java.util.List;
 import mate.academy.rickandmorty.model.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CharacterRepository extends JpaRepository<Character, Long> {
+    @Query("select ch from Character ch where LOWER(ch.name) like LOWER(%?1%)")
     List<Character> findAllByName(String name);
 }
