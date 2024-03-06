cc7e0785cee06f7d8353b050d3530a1262588b1a
Yaroslav Boiko <yaroslav.boiko@ingenios.ro>
2024-03-06 15:34:33 +0200

    Deleted Unused import

--- src/main/java/mate/academy/rickandmorty/model/Character.java
+++ src/main/java/mate/academy/rickandmorty/model/Character.java
@@ -1,6 +1,5 @@
 package mate.academy.rickandmorty.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
