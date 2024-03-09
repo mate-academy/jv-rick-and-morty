dbef54ca24cd37db79a5a58740932348eef58bdf
Roman Soproniuk <151533079+RomanSoproniuk@users.noreply.github.com>
2024-03-09 14:38:58 +0200

    fix bags

--- src/main/java/mate/academy/rickandmorty/exception/EntityNotFoundException.java
+++ src/main/java/mate/academy/rickandmorty/exception/EntityNotFoundException.java
@@ -1,7 +1,6 @@
 package mate.academy.rickandmorty.exception;
 
 public class DataProcessingException extends RuntimeException {
-
     public DataProcessingException(String message) {
         super(message);
     }
